e6eda09ebe9d9d574104b52d12b77411350f0ea1
vselfa <vselfa@ieseljust.com>
2020-01-21 21:59:41 +0100

    Missatge!!!!222

--- app/src/main/java/com/example/dracnegre/Vista/MyCallback.java
+++ app/src/main/java/com/example/dracnegre/Vista/MyCallback.java
@@ -4,7 +4,7 @@ import com.example.dracnegre.Model.ResultatEquip;
 
 import java.util.ArrayList;
 
-public interface MyCallback {
+public interface :MyCallback {
         void onCallback(ArrayList<ResultatEquip> equips);
         //int totalEquips(ArrayList<Punts> equips);
 }
